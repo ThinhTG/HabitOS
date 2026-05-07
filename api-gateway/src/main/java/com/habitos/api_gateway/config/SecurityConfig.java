870bff6251058cd9d1d9a52c7d0397fb2c211bcc
Gia Thinh <thinhtgse173566@fpt.edu.vn>
2026-05-07 12:15:11 +0700

    fix: CORS + Security (API GATEWAY). CHECKED

---
+++ api-gateway/src/main/java/com/habitos/api_gateway/config/SecurityConfig.java
@@ -0,0 +1,29 @@
+package com.habitos.api_gateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityWebFilterChain springSecurityFilterChain(
+            ServerHttpSecurity http
+    ) {
+
+        return http
+                .csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .cors(cors -> {})
+                .authorizeExchange(exchange -> exchange
+                        .pathMatchers("/api/auths/**").permitAll()
+                        .anyExchange().authenticated()
+                )
+                .build();
+    }
+}
